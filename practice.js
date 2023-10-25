e7d00c4c962e3230e1442188274fa09ba4dce280
Sumit singh <sumit700490@gmail.com>
2023-10-25 16:14:02 +0530

    js file updated

---
+++ practice.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a+b
+
+}
+let result=add(1,2)
+
+function multiply(c){
+    return c*2
+}
+let result2=multiply(result);
+
+function divide(x){
+    return x/2
+}
+let result3=divide(result2);
+
+function subtract(g){
+    console.log(g-1)
+}
+subtract(result3)
